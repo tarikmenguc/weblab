be1927535f3204df50d2b212499737216b27e1b2
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2026-02-17 15:50:59 +0300

    yukledim
    yukledim

---
+++ weblab-staging/lab1/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+return (
+<div>
+<h1>Web Tasarimi ve Programlama </h1>
+<h2>LAB -1</h2>
+<p>Ad Soyad: [Muhammed Tarik Menguc]</p>
+<p>Ogrenci No: [23554114] </p>
+</div>
+ )
+ }
+
+export default App
